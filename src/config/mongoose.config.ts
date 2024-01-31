3fa6ea562b08cb848b18528360d9857e24d4b26a
PMtHk <zooby88@naver.com>
2024-01-31 21:32:46 +0900

    feat : add generateByCategory route

--- src/config/mongoose.config.ts
+++ src/config/mongoose.config.ts
@@ -13,7 +13,7 @@ class MongoManager {
       this.connection = await mongoose.connect(uri, { ...options });
       console.log('>> MongoDB connected.');
     } catch (error) {
-      console.log('[MONGODB_Error] MongoDB connect failed. ', error);
+      console.log('[ERROR_MONGO_CONNECT]', error);
     }
   }
 
@@ -24,7 +24,7 @@ class MongoManager {
         console.log('>> MongoDB disconnected.');
       }
     } catch (error) {
-      console.log('[MONGODB_Error] MongoDB disconnect failed. ', error);
+      console.log('[ERROR_MONGO_DISCONNECT]', error);
     }
   }
 }
--- src/controllers/word/word.controller.ts
+++ src/controllers/word/word.controller.ts
@@ -1,20 +1,28 @@
 import { Request, Response } from 'express';
 import { WordService } from '../../services/word.service';
 import Controller, { Methods } from '../controller';
+import { TestService } from '../../services/test.service';
+import { Category } from '../../types/category.type';
 
 export class WordController extends Controller {
   path = '/word';
   routes = [
     {
-      path: '/',
-      method: Methods.POST,
-      handler: this.triggerWordGeneration,
+      path: '/health',
+      method: Methods.GET,
+      handler: this.healthCheck,
       localMiddleware: [],
     },
     {
-      path: '/health',
-      method: Methods.GET,
-      handler: this.health,
+      path: '/generate',
+      method: Methods.POST,
+      handler: this.generateWord,
+      localMiddleware: [],
+    },
+    {
+      path: '/generateByCat',
+      method: Methods.POST,
+      handler: this.generateWordByCat,
       localMiddleware: [],
     }
   ];
@@ -23,21 +31,60 @@ export class WordController extends Controller {
     super();
   }
 
-  async triggerWordGeneration(req: Request, res: Response): Promise<void> {
-    const { count } = req.body;
-
+  async generateWord(req: Request, res: Response): Promise<void> {
     try {
+      const { count } = req.body;
+
+      if (!count) {
+        res.status(400).send('count is required');
+        return;
+      }
+
       const wordService = new WordService(1.1);
       await wordService.loadAllCat(count);
 
       res.status(200).send('done');
+      return;
     } catch (error: any) {
       res.status(500).send(error);
-      console.log(error);
+      console.log('[ERROR_generateWord]', error);
     }
   }
-  
-  async health(req: Request, res: Response): Promise<void> {
-    res.status(200).send('OK');
+
+  async generateWordByCat(req: Request, res: Response): Promise<void> {
+    try {
+      const { count, category } = req.body;
+
+      if (!count || !category) {
+        res.status(400).send('count and category are required');
+        return;
+      }
+
+      if (!Object.values(Category).includes(category)) {
+        res.status(400).send('invalid category');
+        return;
+      }
+
+      const wordService = new WordService(1.1);
+      await wordService.loadACat(category, count);
+
+      res.status(200).send('done');
+      return;
+    } catch (error: any) {
+      res.send(500).send(error);
+      console.log('[ERROR_generateWordByCat]', error);
+    }
+  }
+
+  async healthCheck(req: Request, res: Response): Promise<void> {
+    try {
+      const testService = new TestService();
+
+      res.status(200).send(testService.performTest());
+      return;
+    } catch (error: any) {
+      res.status(500).send(error);
+      console.log('[ERROR_healthCheck]', error);
+    }
   }
 }
